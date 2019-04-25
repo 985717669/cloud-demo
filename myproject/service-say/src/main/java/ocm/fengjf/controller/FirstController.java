708c0b974c6a3b6c439b979e94833bd772f2b0cf
fengjf <12345123>
2019-04-25 14:16:25 +0800

    实现了spring cloud bus 消息总线， 实现配置更新

--- myproject/service-say/src/main/java/ocm/fengjf/controller/FirstController.java
+++ myproject/service-say/src/main/java/ocm/fengjf/controller/FirstController.java
@@ -3,6 +3,7 @@ package ocm.fengjf.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,15 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
  **/
 @RestController
 @Api(description = "测试")
+@RefreshScope
 public class FirstController {
 
 
     @Value("${server.port}")
     String port;
 
+    @Value("${foo.name}")
+    String fooName;
+
     @ApiOperation(value = "say Hello", notes = "")
     @GetMapping("/hi")
     public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
-        return "hi " + name + " ,i am from port:" + port;
+        return "hi " + name + " ,i am from port:" + port + "  name :" + fooName;
     }
 }
