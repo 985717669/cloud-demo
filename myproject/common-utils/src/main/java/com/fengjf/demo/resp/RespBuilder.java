3b258c7655be3224b9205a6b868c8675499f55c2
fengjf <12345123>
2019-06-18 15:25:26 +0800

    excel工具类

--- myproject/common-utils/src/main/java/com/fengjf/demo/resp/RespBuilder.java
+++ myproject/common-utils/src/main/java/com/fengjf/demo/resp/RespBuilder.java
@@ -12,50 +12,47 @@ import org.springframework.http.ResponseEntity;
 public class RespBuilder<T> {
 
 
-    public  static <T> ResponseEntity<T> build() {
+    public  static <T> ResponseEntity<Message<T>> build() {
         return build(BaseResult.SUCCESS);
     }
 
-    public static <T> ResponseEntity<T> build(IRet ret) {
+    public static <T> ResponseEntity<Message<T>> build(IRet ret) {
         return build(ret, HttpStatus.OK);
     }
 
-    public static <T> ResponseEntity<T> build(IRet ret, HttpStatus status) {
+    public static <T> ResponseEntity<Message<T>> build(IRet ret, HttpStatus status) {
         return new ResponseEntity(new Message<>(ret), status);
     }
 
-    public static <T> ResponseEntity<T> build(IRet ret, Object data) {
-        return build(ret, data, HttpStatus.OK);
-    }
-
-    public static <T> ResponseEntity<T> build(IRet ret, String message, HttpStatus status) {
-        return new ResponseEntity(new Message<>(ret, message), status);
-    }
-
-    public static <T> ResponseEntity<T> build(IRet ret, Object data, HttpStatus status) {
-        return new ResponseEntity(new Message<>(ret, data), status);
-    }
-
-    public static <T> ResponseEntity<T> build(Object body, HttpStatus status) {
-        return new ResponseEntity(new Message<>(body), status);
-    }
-
-    public static <T> ResponseEntity<T> build(Message<?> message) {
-        return build(message, HttpStatus.OK);
-    }
-
-    public static <T> ResponseEntity<T> build(Message<?> message, HttpStatus status) {
-        return new ResponseEntity(message, status);
-    }
-
-    public static <T> ResponseEntity<T> build(T data) {
+    public static <T> ResponseEntity<Message<T>> build(T data) {
         if (data == null) {
             return build();
         }
         return build(data, HttpStatus.OK);
     }
 
-    public static <T> ResponseEntity<T> build(String code, String errorMessage, HttpStatus status) {
+    public static <T> ResponseEntity<T> build(IRet ret, String message, HttpStatus status) {
+        return new ResponseEntity(new Message<>(ret, message), status);
+    }
+
+    public static <T> ResponseEntity<Message<T>> build(IRet ret, Object data, HttpStatus status) {
+        return new ResponseEntity(new Message<>(ret, data), status);
+    }
+
+    public static <T> ResponseEntity<Message<T>> build(Object body, HttpStatus status) {
+        return new ResponseEntity(new Message<>(body), status);
+    }
+
+    public static <T> ResponseEntity<Message<T>> build(Message<?> message) {
+        return build(message, HttpStatus.OK);
+    }
+
+    public static <T> ResponseEntity<Message<T>> build(Message<?> message, HttpStatus status) {
+        return new ResponseEntity(message, status);
+    }
+
+
+    public static <T> ResponseEntity<Message<T>> build(String code, String errorMessage, HttpStatus status) {
         Message<?> message = new Message<>();
         message.setCode(code);
         message.setMessage(errorMessage);
